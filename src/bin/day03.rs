1d227f16995c4aca82a82bfe6092e13ef9098c1f
Christian <choumarin@users.noreply.github.com>
2023-12-04 19:19:23 +0000

    day03 part2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,6 +1,8 @@
+use std::collections::{HashMap, HashSet};
+
 const INPUT: &str = include_str!("day03/input.txt");
 
-#[derive(Default, PartialEq, Eq, Debug)]
+#[derive(Default, PartialEq, Eq, Debug, Hash)]
 struct PartNum {
     val: i32,
     line: i32,
@@ -8,8 +10,19 @@ struct PartNum {
     len: i32,
 }
 
+#[derive(PartialEq, Eq, Debug, Hash)]
+struct Gear {
+    line: i32,
+    col: i32,
+}
+
+struct Coord {
+    line: i32,
+    col: i32,
+}
+
 impl PartNum {
-    fn adj_chars(&self, text: &str) -> Vec<char> {
+    fn adj_chars(&self, text: &str) -> Vec<(Coord, char)> {
         let mut adj = Vec::new();
         if self.line > 0 {
             let line = text.lines().nth((self.line - 1) as usize).unwrap();
@@ -17,7 +30,15 @@ impl PartNum {
                 line.chars()
                     .enumerate()
                     .filter(|(i, _)| *i as i32 >= self.col - 1 && *i as i32 <= self.col + self.len)
-                    .map(|(_, c)| c),
+                    .map(|(i, c)| {
+                        (
+                            Coord {
+                                line: self.line - 1,
+                                col: i as i32,
+                            },
+                            c,
+                        )
+                    }),
             );
         }
         let line = text.lines().nth((self.line) as usize).unwrap();
@@ -25,14 +46,30 @@ impl PartNum {
             line.chars()
                 .enumerate()
                 .filter(|(i, _)| *i as i32 == self.col - 1 || *i as i32 == self.col + self.len)
-                .map(|(_, c)| c),
+                .map(|(i, c)| {
+                    (
+                        Coord {
+                            line: self.line,
+                            col: i as i32,
+                        },
+                        c,
+                    )
+                }),
         );
         if let Some(line) = text.lines().nth((self.line + 1) as usize) {
             adj.extend(
                 line.chars()
                     .enumerate()
                     .filter(|(i, _)| *i as i32 >= self.col - 1 && *i as i32 <= self.col + self.len)
-                    .map(|(_, c)| c),
+                    .map(|(i, c)| {
+                        (
+                            Coord {
+                                line: self.line + 1,
+                                col: i as i32,
+                            },
+                            c,
+                        )
+                    }),
             );
         }
         adj
@@ -41,7 +78,21 @@ impl PartNum {
     fn has_adj_symbols(&self, text: &str) -> bool {
         self.adj_chars(text)
             .iter()
-            .any(|c| !c.is_digit(10) && *c != '.')
+            .any(|(_, c)| !c.is_ascii_digit() && *c != '.')
+    }
+
+    fn gears(&self, text: &str) -> Vec<Gear> {
+        self.adj_chars(text)
+            .iter()
+            .enumerate()
+            .filter_map(|(_, (coord, c))| match c {
+                '*' => Some(Gear {
+                    col: coord.col,
+                    line: coord.line,
+                }),
+                _ => None,
+            })
+            .collect()
     }
 }
 
@@ -57,7 +108,10 @@ ghi";
         val: 1,
     };
     assert_eq!(
-        p.adj_chars(text),
+        p.adj_chars(text)
+            .iter()
+            .map(|(_, x)| *x)
+            .collect::<Vec<_>>(),
         vec!['a', 'b', 'c', 'd', 'f', 'g', 'h', 'i']
     );
 }
@@ -66,7 +120,7 @@ fn parts_in_str(line: i32, text: &str) -> Vec<PartNum> {
     let mut parts = Vec::new();
     let mut op: Option<PartNum> = None;
     for (i, c) in text.chars().enumerate() {
-        if c.is_digit(10) {
+        if c.is_ascii_digit() {
             if let Some(ref mut p) = op {
                 p.len += 1;
                 p.val *= 10;
@@ -79,16 +133,13 @@ fn parts_in_str(line: i32, text: &str) -> Vec<PartNum> {
                     len: 1,
                 });
             }
-        } else {
-            if let Some(p) = op {
-                parts.push(p);
-                op = None;
-            }
+        } else if let Some(p) = op {
+            parts.push(p);
+            op = None;
         }
     }
     if let Some(p) = op {
         parts.push(p);
-        op = None;
     }
     parts
 }
@@ -119,15 +170,29 @@ fn part1(input: &str) -> i32 {
     input
         .lines()
         .enumerate()
-        .map(|(i, l)| parts_in_str(i as i32, l))
-        .flatten()
+        .flat_map(|(i, l)| parts_in_str(i as i32, l))
         .filter(|p| p.has_adj_symbols(input))
         .map(|p| p.val)
         .sum()
 }
 
-fn part2(input: &str) -> u32 {
-    unimplemented!()
+fn part2(input: &str) -> i32 {
+    let parts = input
+        .lines()
+        .enumerate()
+        .flat_map(|(i, l)| parts_in_str(i as i32, l))
+        .collect::<Vec<_>>();
+    let mut gears: HashMap<Gear, HashSet<&PartNum>> = HashMap::new();
+    for part in parts.iter() {
+        for gear in part.gears(input) {
+            gears.entry(gear).or_default().insert(part);
+        }
+    }
+    gears
+        .iter()
+        .filter(|(_, parts)| parts.len() >= 2)
+        .map(|(_, parts)| parts.iter().map(|p| p.val).product::<i32>())
+        .sum()
 }
 
 #[cfg(test)]
