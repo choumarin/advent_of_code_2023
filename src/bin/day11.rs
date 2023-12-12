1218da70fa6e4cb8d735a489d274fc05cd6146ec
Christian <choumarin@users.noreply.github.com>
2023-12-12 18:31:54 +0000

    day11 part2

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -51,36 +51,63 @@ impl Universe {
             .collect()
     }
 
-    fn expand(&mut self) {
+    fn galaxies_lines_cols(&self) -> (HashSet<i64>, HashSet<i64>) {
         let mut cols_with_galaxies = HashSet::new();
         let mut lines_with_galaxies = HashSet::new();
         for (line, content) in self.0.iter().enumerate() {
             let mut galaxies_in_line = false;
             for (col, space) in content.iter().enumerate() {
                 if *space == Space::Galaxy {
-                    cols_with_galaxies.insert(col);
+                    cols_with_galaxies.insert(col as i64);
                     galaxies_in_line = true;
                 }
             }
             if galaxies_in_line {
-                lines_with_galaxies.insert(line);
+                lines_with_galaxies.insert(line as i64);
             }
         }
+        (lines_with_galaxies, cols_with_galaxies)
+    }
+
+    fn expand(&mut self, factor: i64) {
+        let (lines_with_galaxies, cols_with_galaxies) = self.galaxies_lines_cols();
         // insert lines
         let len = self.0.len();
         for i in (0..len).rev() {
-            if !lines_with_galaxies.contains(&i) {
-                self.0.insert(i, self.empty_line())
+            if !lines_with_galaxies.contains(&(i as i64)) {
+                for _ in 0..(factor - 1) {
+                    self.0.insert(i, self.empty_line())
+                }
             }
         }
         // insert cols
         for line in self.0.iter_mut() {
             let len = line.len();
             for i in (0..len).rev() {
-                if !cols_with_galaxies.contains(&i) {
-                    line.insert(i, Space::Empty);
+                if !cols_with_galaxies.contains(&(i as i64)) {
+                    for _ in 0..(factor - 1) {
+                        line.insert(i, Space::Empty);
+                    }
                 }
             }
+        }
+    }
+
+    fn expand_coords(&self, coords: Coords, factor: i64) -> Coords {
+        let (lines_with_galaxies, cols_with_galaxies) = self.galaxies_lines_cols();
+        let lines = lines_with_galaxies
+            .iter()
+            .filter(|&&i| i < coords.line)
+            .count() as i64;
+        let new_line = (coords.line - lines) * (factor - 1) + coords.line;
+        let cols = cols_with_galaxies
+            .iter()
+            .filter(|&&i| i < coords.col)
+            .count() as i64;
+        let new_col = (coords.col - cols) * (factor - 1) + coords.col;
+        Coords {
+            line: new_line,
+            col: new_col,
         }
     }
 
@@ -116,7 +143,7 @@ fn test_expand() {
 #....#.......";
 
     let mut universe = input.parse::<Universe>().unwrap();
-    universe.expand();
+    universe.expand(2);
 
     assert_eq!(universe, Universe::from(expanded));
 }
@@ -134,7 +161,7 @@ fn test_galaxies() {
 .......#..
 #...#.....";
     let mut universe = input.parse::<Universe>().unwrap();
-    universe.expand();
+    universe.expand(2);
     let galaxies = universe.galaxies();
     assert_eq!(galaxies[4].dist_square(&galaxies[8]), 9);
     assert_eq!(galaxies[0].dist_square(&galaxies[6]), 15);
@@ -157,12 +184,14 @@ fn test_uniq_pairs() {
 }
 
 fn part1(input: &str) -> i64 {
-    let mut universe = input.parse::<Universe>().unwrap();
-    universe.expand();
-    let galaxies = universe.galaxies();
+    let universe = input.parse::<Universe>().unwrap();
+    let mut galaxies = universe.galaxies();
+    for g in galaxies.iter_mut() {
+        *g = universe.expand_coords(*g, 2);
+    }
     let mut total_distance = 0;
     let pairs = uniq_pairs(galaxies);
-    for pair in pairs{
+    for pair in pairs {
         let dist = pair.0.dist_square(&pair.1);
         total_distance += dist;
     }
@@ -170,7 +199,18 @@ fn part1(input: &str) -> i64 {
 }
 
 fn part2(input: &str) -> i64 {
-    unimplemented!()
+    let universe = input.parse::<Universe>().unwrap();
+    let mut galaxies = universe.galaxies();
+    for g in galaxies.iter_mut() {
+        *g = universe.expand_coords(*g, 1_000_000);
+    }
+    let mut total_distance = 0;
+    let pairs = uniq_pairs(galaxies);
+    for pair in pairs {
+        let dist = pair.0.dist_square(&pair.1);
+        total_distance += dist;
+    }
+    total_distance
 }
 
 #[cfg(test)]
@@ -195,7 +235,7 @@ mod test {
 
     #[test]
     fn test_parse2() {
-        assert_eq!(part2(TEST_INPUT_1), 4);
+        assert_eq!(part2(TEST_INPUT_1), 82000210);
     }
 }
 
